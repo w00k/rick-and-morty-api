d4fe9ea12b186c46e363c072908e73ccad904c59
w00k <wook82@gmail.com>
2021-10-02 22:06:13 -0300

    primer commit con fuentes

---
+++ rickandmorty_acl/src/main/java/com/wook/rickandmorty/acl/RickandmortyAclApplication.java
@@ -0,0 +1,13 @@
+package com.wook.rickandmorty.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickandmortyAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickandmortyAclApplication.class, args);
+	}
+
+}
---
+++ rickandmorty_acl/src/test/java/com/wook/rickandmorty/acl/RickandmortyAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wook.rickandmorty.acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickandmortyAclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
